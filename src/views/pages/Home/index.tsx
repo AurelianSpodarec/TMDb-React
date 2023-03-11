d40fead0d5efae76a277d9142b48c14b17bc6252
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-11 19:03:14 +0000

    feat: Added services; default request code as well as added 'themovedb' API and movie requests

--- src/views/pages/Home/index.tsx
+++ src/views/pages/Home/index.tsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import { getMovieListPopular } from "../../../services/themoviedb/api/Movies";
+
 function Home() {
+
+    async function fetch() {
+        const res = await getMovieListPopular()
+        console.log(res)
+    }
+    
+    useEffect(() => {
+        fetch()
+    }, [])
+
     return (
         <div>
             <h1 className="text-5xl font-bold flex text-red-500">Home Page</h1>

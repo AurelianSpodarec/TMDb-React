a360dd0211395ddd37e5ebce99f9563290f5dc7c
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-11 20:19:23 +0000

    chore: Changed 'movie_id' to 'id'

--- src/services/themoviedb/api/Movies.tsx
+++ src/services/themoviedb/api/Movies.tsx
@@ -4,80 +4,80 @@
 import FetchTheMovieDB from "../fetch/FetchTheMovieDB"
 
 interface MovieRequestParams {
-    movie_id: number;
+    id: number;
 }
   
 
-export async function getMovieDetails({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}`, "GET")
+export async function getMovieDetails({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}`, "GET")
 }
 
-export async function getMovieAccountStates({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/account_states`, "GET")
+export async function getMovieAccountStates({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/account_states`, "GET")
 }
 
-export async function getMovieAlternativeTitles({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/alternative_titles`, "GET")
+export async function getMovieAlternativeTitles({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/alternative_titles`, "GET")
 }
 
-export async function getMovieChanges({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/changes`, "GET")
+export async function getMovieChanges({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/changes`, "GET")
 }
 
-export async function getMovieCredits({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/credits`, "GET")
+export async function getMovieCredits({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/credits`, "GET")
 }
 
-export async function getMovieExternalIDs({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/external_ids`, "GET")
+export async function getMovieExternalIDs({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/external_ids`, "GET")
 }
 
-export async function getMovieImages({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/images`, "GET")
+export async function getMovieImages({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/images`, "GET")
 }
 
-export async function getMovieKeywords({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/keywords`, "GET")
+export async function getMovieKeywords({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/keywords`, "GET")
 }
 
-export async function getMovieLists({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/lists`, "GET")
+export async function getMovieLists({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/lists`, "GET")
 }
 
-export async function getMovieRecommendations({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/recommendations`, "GET")
+export async function getMovieRecommendations({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/recommendations`, "GET")
 }
 
-export async function getMovieReleaseDates({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/release_dates`, "GET")
+export async function getMovieReleaseDates({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/release_dates`, "GET")
 }
 
-export async function getMovieReviews({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/reviews`, "GET")
+export async function getMovieReviews({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/reviews`, "GET")
 }
 
-export async function getMovieSimilarMovies({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/similar`, "GET")
+export async function getMovieSimilarMovies({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/similar`, "GET")
 }
 
-export async function getMovieTranslations({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/translations`, "GET")
+export async function getMovieTranslations({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/translations`, "GET")
 }
 
-export async function getMovieVideos({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/videos`, "GET")
+export async function getMovieVideos({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/videos`, "GET")
 }
 
-export async function getMovieWatchProviders({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/watch/providers`, "GET")
+export async function getMovieWatchProviders({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/watch/providers`, "GET")
 }
 
-export async function postMovieRate({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/rating`, "POST")
+export async function postMovieRate({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/rating`, "POST")
 }
 
-export async function deleteMovieRate({ movie_id }:MovieRequestParams): Promise<any> {
-    return FetchTheMovieDB(`movie/${movie_id}/rating`, "DELETE")
+export async function deleteMovieRate({ id }:MovieRequestParams): Promise<any> {
+    return FetchTheMovieDB(`movie/${id}/rating`, "DELETE")
 }
 
 export async function getMovieLatest(): Promise<any> {
@@ -92,8 +92,8 @@ export async function getMovieListPopular(page = 1): Promise<any>  {
     const params:any = {
         page
     }
+
     const qa = new URLSearchParams(params);
-    
     const url = qa.toString() === "" ? "" : `?${qa}`;
     return FetchTheMovieDB(`movie/popular${url}`, "GET")
 }

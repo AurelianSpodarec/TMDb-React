806e2061db2a19c2fba696ea8889c4c2cd16a99d
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-12 11:20:06 +0000

    update: Added some links to the header

--- src/views/pages/_components/Header/Header.tsx
+++ src/views/pages/_components/Header/Header.tsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <header>
             Header
+
+            <ul>
+                <li>
+                    <Link to="">Discover</Link>
+                </li>
+                <li>
+                    <Link to="/movies">Movies</Link>
+                </li>
+                <li>
+                    <Link to="">Trending</Link>
+                </li>
+                <li>
+                    <Link to="">People</Link>
+                </li>
+                <li>
+                    <Link to="">Search</Link>
+                </li>
+                <li>
+                    <Link to="">Discover</Link>
+                </li>
+                <li>
+                    <Link to="">TV</Link>
+                </li>
+            </ul>
         </header>
     )
 }

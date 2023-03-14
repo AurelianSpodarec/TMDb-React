8595487857f2ce94db7613a8a3d93343eed7f621
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-14 10:36:21 +0000

    improve: Improved TS for MediaType

--- src/views/atoms/MediaType.tsx
+++ src/views/atoms/MediaType.tsx
@@ -1,22 +1,20 @@
 import { capitalizeFirstLetter } from "@/utils/common";
 
-function MediaType({ name = "movie" }:{ name?: keyof mediaTypes }) {
 
-    const mediaTypes: mediaTypes = {
+function MediaType({ name = "movie" }: { name?: MediaTypes }) {
+
+    const mediaTypes: Record<MediaTypes, string> = {
         movie: "border-yellow-400",
-        tv: "border-red-400",
-    }
+        tv: "border-red-400"
+    };
 
     return (
         <div className={`border text-center inline-block py-1 px-2 rounded-lg ${mediaTypes[name]}`}>
             <span className="text-white">{capitalizeFirstLetter(name)}</span>
         </div>
-    )
+    );
 }
 
 export default MediaType;
 
-interface mediaTypes {
-    movie: string;
-    tv: string;
-}
+type MediaTypes = "movie" | "tv";

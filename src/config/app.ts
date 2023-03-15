1136396189930682f2c98ea4ef779c2542fb2f83
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-15 11:28:54 +0000

    fix: Fixed the wrong url for size, removed - between w and value

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,16 +1,16 @@
 export const appConfig =  {
     urlImageTMDB: "https://image.tmdb.org/t/p",
     urlImageSizeTMBD: {
-        "xs": "/w-45",
-        "sm": "/w-92", 
-        "md": "/w-154", 
-        "lg": "/w-185", 
-        "xl": "/w-300", 
-        "2xl": "/w-342", 
-        "3xl": "/w-500", 
-        "4xl": "/w-600", 
-        "5xl": "/w-780", 
-        "6xl": "/w-1280", 
+        "xs": "/w45",
+        "sm": "/w92", 
+        "md": "/w154", 
+        "lg": "/w185", 
+        "xl": "/w300", 
+        "2xl": "/w342", 
+        "3xl": "/w500", 
+        "4xl": "/w600", 
+        "5xl": "/w780", 
+        "6xl": "/w1280", 
         "original": "/original"
     }
 }   

8f466df7325c486b3774578a70929650a6f7a96f
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-15 11:14:02 +0000

    chore: Updated url image size to come from app config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,16 +1,16 @@
 export const appConfig =  {
     urlImageTMDB: "https://image.tmdb.org/t/p",
     urlImageSizeTMBD: {
-        "xs": "w-45",
-        "sm": "w-92", 
-        "md": "w-154", 
-        "lg": "w-185", 
-        "xl": "w-300", 
-        "2xl": "w-342", 
-        "3xl": "w-500", 
-        "4xl": "w-600", 
-        "5xl": "w-780", 
-        "6xl": "w-1280", 
-        "original": "original"
+        "xs": "/w-45",
+        "sm": "/w-92", 
+        "md": "/w-154", 
+        "lg": "/w-185", 
+        "xl": "/w-300", 
+        "2xl": "/w-342", 
+        "3xl": "/w-500", 
+        "4xl": "/w-600", 
+        "5xl": "/w-780", 
+        "6xl": "/w-1280", 
+        "original": "/original"
     }
 }   

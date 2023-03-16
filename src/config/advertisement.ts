034ece5c7d8891bc9af9afb8de3c94855ee5d4f3
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-16 18:09:36 +0000

    feat: Added advertisement images

---
+++ src/config/advertisement.ts
@@ -0,0 +1,12 @@
+export const configAdvertisement = [
+    {
+        type: "advert",
+        name: "Nandos Ticket",
+        poster_path: "nandos-movie-ticket.png"
+    },
+    {
+        type: "advert",
+        name: "Airbnb Ticket",
+        poster_path: "airbnb-movie-ticket.png"
+    }
+]

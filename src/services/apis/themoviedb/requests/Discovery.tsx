0177bc76ec595a201ab0c4a99ccb021e90bb413c
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-16 13:06:58 +0000

    chore: Added Discovery for movies and empty file for TV api

---
+++ src/services/apis/themoviedb/requests/Discovery.tsx
@@ -0,0 +1,53 @@
+// ============================================================
+// API Docs: https://developers.themoviedb.org/3/discover/movie-discover
+// ============================================================
+import FetchTheMovieDB from "../fetch/FetchTheMovieDB"
+
+export async function getDiscoverMovie(queryParams: DiscoverMovieQueryParams): Promise<any>  {
+    const params:any = {...queryParams}
+
+    const qa = new URLSearchParams(params);
+    const url = qa.toString() === "?" ? "" : `?${qa}&`;
+    return FetchTheMovieDB(`discover/movie${url}`, "GET")
+}
+
+interface DiscoverMovieQueryParams {
+    "language"?: string;
+    "region"?: string;
+    "sort_by"?: string;
+    "certification_country"?: string;
+    "certification"?: string;
+    "certification.lte"?: string;
+    "certification.gte"?: string;
+    "include_adult"?: boolean;
+    "include_video"?: boolean;
+    "page"?: number;
+    "primary_release_year"?: number;
+    "primary_release_date.gte"?: string;
+    "primary_release_date.lte"?: string;
+    "release_date.gte"?: string;
+    "release_date.lte"?: string;
+    "with_release_type"?: number;
+    "year"?: number;
+    "vote_count.gte"?: number;
+    "vote_count.lte"?: number;
+    "vote_average.gte"?: number;
+    "vote_average.lte"?: number;
+    "with_cast"?: string;
+    "with_crew"?: string;
+    "with_people"?: string;
+    "with_companies"?: string;
+    "with_genres"?: string;
+    "with_keywords"?: string;
+    "with_runtime.gte"?: number;
+    "with_runtime.lte"?: number;
+    "with_original_language"?: string;
+    "with_watch_providers"?: string;
+    "watch_region"?: string;
+    "with_watch_monetization_types"?: string;
+    "without_companies"?: string;
+    "without_genres"?: string;
+    "without_keywords"?: string;
+    "without_people"?: string;
+    "without_tv_networks"?: string;
+}
---
+++ src/services/apis/themoviedb/requests/TV.tsx
@@ -0,0 +1,5 @@
+// ============================================================
+// API Docs: https://developers.themoviedb.org/3/tv/get-tv-details
+// ============================================================
+import FetchTheMovieDB from "../fetch/FetchTheMovieDB"
+

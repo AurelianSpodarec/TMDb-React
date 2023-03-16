be9c52e7de0835aa5e482698cd9ea04c1cc7521d
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-16 14:44:33 +0000

    update: Added guaring to some values if they are empty display nothing

--- src/views/atoms/MediaType.tsx
+++ src/views/atoms/MediaType.tsx
@@ -1,13 +1,14 @@
 import { capitalizeFirstLetter } from "@/utils/common";
 
 
-function MediaType({ name = "movie" }: { name?: MediaTypes }) {
+function MediaType({ name }: { name?: MediaTypes }) {
 
     const mediaTypes: Record<MediaTypes, string> = {
         movie: "border-yellow-400",
         tv: "border-red-400"
     };
 
+    if(!name) return <></>
     return (
         <div className={`border text-center inline-block py-1 px-2 rounded-lg ${mediaTypes[name]}`}>
             <span className="text-white">{capitalizeFirstLetter(name)}</span>

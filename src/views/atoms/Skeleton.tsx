d30d2631897fe08ce8f5783435323717dfb5170e
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-17 23:34:42 +0000

    fix: Fixed some UI for skeleton

--- src/views/atoms/Skeleton.tsx
+++ src/views/atoms/Skeleton.tsx
@@ -1,25 +1,25 @@
-function Skeleton({ variant = "rect", width = 100, height = 16, rounded = "none", fontSize = 16 }) {
+function Skeleton({ className, variant = "rect", width = 100, height = 16, radius, fontSize = 16 }:any) {
 
     const skeletonStyles = {
-        width: `${width}px`,
-        height: `${height}px`,
-        borderRadius: `${rounded === "circle" ? "50%" : rounded === "rounded" ? "8px" : "0"}`,
+        width: `${width}`,
+        height: `${height}`,
+        borderRadius: radius,
         fontSize: `${fontSize}px`,
     };
 
     return (
-        <div className="animate-pulse bg-gray-200" style={skeletonStyles}>
-        {variant === "text" && (
-            <div className="h-4 bg-white rounded-md mt-2 mb-1" style={{ width: "50%" }}></div>
-        )}
-        
-        {variant === "circle" && (
-            <div className="bg-white rounded-full" style={{ width: `${height}px`, height: `${height}px` }}></div>
-        )}
+        <div className={`${className} animate-pulse relative`} style={skeletonStyles}>
+            {variant === "text" && (
+                <div className={`${className} bg-white rounded-xl`} style={{ width: `${width}`, height: `${height}` }}></div>
+            )}
 
-        {variant === "rect" && (
-            <div className="bg-white rounded-md" style={{ width: "100%", height: "100%" }}></div>
-        )}
+            {variant === "circle" && (
+                <div className={`${className} bg-white rounded-full`} style={{ width: `${width}`, height: `${height}` }}></div>
+            )}
+
+            {variant === "rect" && (
+                <div className={` bg-white rounded-xl`}  style={{ width: `${width}`, height: `${height}` }}></div>
+            )} 
         </div>
     );
 }

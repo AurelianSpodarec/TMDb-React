2f456882069c6c0dd62a3258566bd062713054aa
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-17 09:58:09 +0000

    chore: Added some filter options

---
+++ src/config/filters/voteCount.tsx
@@ -0,0 +1,26 @@
+export default {}
+
+
+// vote_count.gte
+// integer
+// Filter and only include movies that have a vote count that is greater or equal to the specified value.
+
+// minimum: 0
+// optional
+// vote_count.lte
+// integer
+// Filter and only include movies that have a vote count that is less than or equal to the specified value.
+
+// minimum: 1
+// optional
+// vote_average.gte
+// number
+// Filter and only include movies that have a rating that is greater or equal to the specified value.
+
+// minimum: 0
+// optional
+// vote_average.lte
+// number
+// Filter and only include movies that have a rating that is less than or equal to the specified value.
+
+// minimum: 0

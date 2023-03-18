d450ffdba2e03252f03f5bf322d52943d81de0cb
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-18 10:39:11 +0000

    chore: Added `hooks` folder to Alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src/"),
             assets: `${path.resolve(__dirname, "./src/assets/")}`,
             config: `${path.resolve(__dirname, "./src/config/")}`,
+            hooks: `${path.resolve(__dirname, "./src/hooks/")}`,
             routes: `${path.resolve(__dirname, "./src/routes/")}`,
             services: `${path.resolve(__dirname, "./src/services/")}`,
             types: `${path.resolve(__dirname, "./src/types/")}`,
@@ -17,7 +18,7 @@ export default defineConfig({
             views: `${path.resolve(__dirname, "./src/views/")}`,
             atoms: `${path.resolve(__dirname, "./src/views/atoms/")}`,
             molecules: `${path.resolve(__dirname, "./src/views/molecules/")}`,
-            features: `${path.resolve(__dirname, "./src/views/features/")}`,
+            features: `${path.resolve(__dirname, "./src/views/_features/")}`,
         }
     }
 })

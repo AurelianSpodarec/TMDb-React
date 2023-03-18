b0246b516312561e8202cc5e1240eb456656e048
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-18 23:02:42 +0000

    refactor: Cleaned up the paginatoin and abstracted a few value elements for the UI

--- src/views/molecules/Pagination/Pagination.tsx
+++ src/views/molecules/Pagination/Pagination.tsx
@@ -1,28 +1,35 @@
 import usePagination from "./usePagination";
 import PageItem from "./_components/PageItem";
 
-//TODO: On page change snap to the top
 function Pagination({ data }:any) {
     if(!data) return <></>
-    const currentPage = data.page;
-    const totalPages = data.total_pages;
-    const pager = usePagination({ currentPage, totalPages});
+    const pager = usePagination(data);
 
     return (
         <div className="text-white">
         <div className="flex justify-center items-center">
         
-            <button type="button" aria-label="Pagination: Previous page" onClick={pager.goToPreviousPage} className={`rounded h-14 px-6 ${pager.currentPage === 1 ? "cursor-not-allowed bg-yellow-900" : "bg-yellow-700"} `}>
+            <button 
+                type="button" 
+                aria-label="Pagination: Previous page" 
+                onClick={pager.goToPreviousPage} 
+                className={`rounded h-14 px-6 ${pager.firstPage ? "cursor-not-allowed bg-yellow-900" : "bg-yellow-700"} `}
+            >
                 Prev
             </button>
 
             <div className="flex">
                 <PageItem onClick={() => pager.goToPage("1")} label="1" />
-                <PageItem activePage={currentPage} label={currentPage} />
-                <PageItem onClick={() => pager.goToPage(totalPages)} label={totalPages} />
+                <PageItem activePage={pager.currentPage} label={pager.currentPage} />
+                <PageItem onClick={() => pager.goToPage(pager.totalPages)} label={pager.totalPages} />
             </div>
 
-            <button type="button" aria-label="Pagination: Next Page" onClick={pager.goToNextPage} className={`rounded h-14 px-6 ${pager.currentPage === totalPages ? "cursor-not-allowed bg-yellow-900" : "bg-yellow-700"} `}>
+            <button 
+                type="button" 
+                aria-label="Pagination: Next Page" 
+                onClick={pager.goToNextPage} 
+                className={`rounded h-14 px-6 ${pager.lastPage ? "cursor-not-allowed bg-yellow-900" : "bg-yellow-700"} `}
+            >
                 Next
             </button>
 
--- src/views/molecules/Pagination/usePagination.tsx
+++ src/views/molecules/Pagination/usePagination.tsx
@@ -3,13 +3,25 @@ import { useLocation, useSearchParams } from "react-router-dom";
 
 
 
-const usePagination = ({ activePage, totalPages }:any) => {
+const usePagination = (data:any) => {
+    if(!data) return <></>
+    
     let [searchParams, setSearchParams] = useSearchParams();
-    let [currentPage, setCurrentPage] = useState();
-
+    
     const location = useLocation();
     const existingSearchParams = new URLSearchParams(location.search);
     const pageValue = parseInt(existingSearchParams.get('page') || 1);
+    
+    
+    const totalPages = data && data.total_pages;
+    const currentPage = data && data.page
+
+    const firstPage = currentPage === 1;
+    const lastPage = currentPage === data.totalPages;
+
+
+     // Page Helpers 
+    // ==========================================
 
     function snapToTop() {
         window.scrollTo(0, 0);
@@ -18,30 +30,33 @@ const usePagination = ({ activePage, totalPages }:any) => {
     const updateSearchParams = (newPageValue: number) => {
         existingSearchParams.set("page", String(newPageValue));
         setSearchParams(existingSearchParams);
-        setCurrentPage(String(newPageValue));
-        snapToTop();
+        snapToTop()
     };
 
+
+    // Page Functionality 
+    // ==========================================
+
     const goToNextPage = () => {
-        if (pageValue === totalPages) return;
+        if (pageValue === data.totalPages) return;
         updateSearchParams(pageValue + 1);
-        snapToTop();
     };
 
     const goToPreviousPage = () => {
         if(pageValue === 1) return;
         updateSearchParams(pageValue - 1);
-        snapToTop()
     };
     
     const goToPage = ({ page }:any) => {
         existingSearchParams.set("page", String(page));
         setSearchParams(existingSearchParams.toString());
-        snapToTop()
     };
 
     return {
         currentPage,
+        firstPage,
+        lastPage,
+        totalPages,
         goToNextPage,
         goToPreviousPage,
         goToPage
